8007bcf8179c7d02e85823a8b8729fad5bb41c23
liuDam1 <liu.jiangyu.dam@gmail.com>
2025-09-29 02:36:47 +0200

    Lanzar javaFx ok

--- src/main/java/es/etg/psp/prueba/Main.java
+++ src/main/java/es/etg/psp/prueba/Main.java
@@ -1,7 +1,10 @@
 package es.etg.psp.prueba;
 
+import es.etg.psp.prueba.controlador.AlumnoController;
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(AlumnoController.class, args);
     }
 }
---
+++ src/main/java/es/etg/psp/prueba/controlador/AlumnoController.java
@@ -0,0 +1,17 @@
+package es.etg.psp.prueba.controlador;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AlumnoController extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("vista/FormularioAlumno.fxml"));
+        Scene scene = new Scene(loader.load());
+        stage.setTitle("Gestión de Alumnos");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
